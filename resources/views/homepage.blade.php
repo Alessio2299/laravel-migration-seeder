6cf5999f8111d2832095209515f2fb5ac673adc3
Alessio Napoli <alessionapoli99@icloud.com>
2022-03-31 16:02:33 +0200

    Ended Exercise

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,23 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Homepage</title>
-    </head>
-    <body>
-        <h1>Homepage</h1>
-        <ul>
-            @foreach ($travels as $travel)
-            <li>{{$travel->name}}</li>
-            <li>{{$travel->image}}</li>
-            <li>{{$travel->description}}</li>
-            <li>{{$travel->place}}</li>
-            <li>{{$travel->date}}</li>
-            <li>{{$travel->price}}</li>
-            <li>{{$travel->available}}</li>
-            @endforeach
-        </ul>
-    </body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <title>Homepage</title>
+  </head>
+  <body>
+    <h1>Homepage</h1>
+    <div class="content">
+      @foreach ($travels as $travel)
+      <div class="cardTravel">
+        <h2>{{$travel->name}}</h2>
+        <img src="{{$travel->image}}" alt="{{$travel->place}}">
+        <div class="info">
+          <span class="place">{{$travel->place}}</span>
+          <span class="date">{{$travel->date}}</span>
+          @if ($travel->available == 1)
+            {{$travel->available = "Available"}}    
+          @else
+            {{$travel->available = "Not Available"}}
+          @endif
+        </div>
+        <p>{{$travel->description}}</p>
+        <div class="buy">
+          <span class="price">{{$travel->price}} €</span>
+          <button>Prenota</button>
+        </div>
+      </div>
+      @endforeach
+    </div>
+
+  </body>
 </html>
